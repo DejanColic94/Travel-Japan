bc8b1b8b759c2b4aae9d9a16cfe02b1621a74bcc
Dejan Colic <dejan.colic99@gmail.com>
2021-06-21 00:46:44 +0200

    Navbar done, pathing problems fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState , useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button}  from './Button';
 
 function Navbar() {
 
@@ -17,55 +19,64 @@ function Navbar() {
         }
     };
 
+    useEffect( ()=> {
+      showButton()
+    },[])
+
     window.addEventListener('resize',showButton);
 
 
     return (
         <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        TRAVEL JAPAN <i class="fas fa-globe-asia"></i>
-                    </Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <i className= {click ? 'fas fa-times' : 'fas fa-bars'} />
-                    </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/aboutJapan' className='nav-links' onClick={closeMobileMenu}>
-                                About Japan
-                            </Link>
-                        </li><li className='nav-item'>
-                            <Link to='/destignations' className='nav-links' onClick={closeMobileMenu}>
-                                Destignations
-                            </Link>
-                            <li className='nav-item'>
-                            <Link to='/maps' className='nav-links' onClick={closeMobileMenu}>
-                                Maps
-                            </Link>
-                            <li className='nav-item'>
-                            <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
-                                Products
-                            </Link>
-                        </li>
-                            <li className='nav-item'>
-                            <Link to='/signUp' className='nav-links' onClick={closeMobileMenu}>
-                                Sign Up!
-                            </Link>
-                        </li>
-                        </li>
-                        </li>
-                    </ul>
-                    {button && <button buttonStyle="btn--outline">Sign Up!</button>}
-                </div>
-            </nav>
+          <nav className='navbar'>
+            <div className='navbar-container'>
+              <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                TRAVEL JAPAN
+                <i class='fas fa-globe-asia' />
+              </Link>
+              <div className='menu-icon' onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+              </div>
+              <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'>
+                  <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                    About Japan
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link
+                    to='/destignations'
+                    className='nav-links'
+                    onClick={closeMobileMenu}
+                  >
+                    Destignations
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link
+                    to='/products'
+                    className='nav-links'
+                    onClick={closeMobileMenu}
+                  >
+                    Products
+                  </Link>
+                </li>
+    
+                <li>
+                  <Link
+                    to='/sign-up'
+                    className='nav-links-mobile'
+                    onClick={closeMobileMenu}
+                  >
+                    Sign Up
+                  </Link>
+                </li>
+              </ul>
+              {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+            </div>
+          </nav>
         </>
-    )
-}
-
-export default Navbar
+      );
+    }
+    
+    export default Navbar;
