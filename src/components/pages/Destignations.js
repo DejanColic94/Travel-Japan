f0dfa0bfb1a4493af6edc5fc9f2fd7a62d35b5bb
DEJAN\38163 <cola_dcx@yahoo.com>
2023-06-12 16:52:13 +0200

    Destignations page

--- src/components/pages/Destignations.js
+++ src/components/pages/Destignations.js
@@ -7,12 +7,17 @@ import nara from '../../images/nara.jpg';
 import okinawa from '../../images/okinawa.jpg';
 import osaka from '../../images/osaka.jpg';
 import sapporo from '../../images/sapporo.jpg';
-import tokyo from '../../images/tokyo.jpg';
+import kobe from '../../images/kobe.jpg';
+import sendai from '../../images/sendai.jpg';
+import kumamoto from '../../images/kumamoto.jpg';
+import asahikawa from '../../images/asahikawa.jpg';
+import nagasaki from '../../images/nagasaki.jpg';
 
 export default function Destignations() {
     return (
-        <div className='cards'>
-          <h1>Check out these EPIC Destinations!</h1>
+        
+        <div className='cards-d'>
+          <h1>Choose from many interesting places Japan has to offer:</h1>
           <div className='cards__container'>
             <div className='cards__wrapper'>
               <ul className='cards__items'>
@@ -49,16 +54,44 @@ export default function Destignations() {
                   path='/destignations'
                 />
                  </ul>
-                <ul className='cards__items'>
+                 <ul className='cards__items'>
                 <CardItem
-                  src={tokyo}
-                  text='Tokyo : Welcome to the biggest city on the planet'
-                  label='Night life'
+                  src={kobe}
+                  text='Kobe : Home of the best wagyu beef in the world'
+                  label='Food'
+                  path='/destignations'
+                />
+                <CardItem
+                  src={sendai}
+                  text='Sendai : Attend one of many interesting festivals which occur during the year'
+                  label='Festivities'
                   path='/destignations'
                 />
               </ul>
+              <ul className='cards__items'>
+                <CardItem
+                  src={kumamoto}
+                  text='Kumamoto : Explore Mount Aso and expirience the deep history of feudal Japan'
+                  label='Mystery'
+                  path='/destignations'
+                />
+                <CardItem
+                  src={asahikawa}
+                  text='Asahikawa : Welcome to the "roof" of Japan, the penguins are awaiting'
+                  label='Adventure'
+                  path='/destignations'
+                />
+                <CardItem
+                  src={nagasaki}
+                  text='Nagasaki : Expirience the best hot springs in Japan'
+                  label='Relaxation'
+                  path='/destignations'
+                />
+                 </ul>
+             
             </div>
           </div>
         </div>
+        
       );
 }
